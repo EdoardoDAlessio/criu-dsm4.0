f79f3f29f74f4a647709a29382a6ed382c313fdf
Edoardo D'Alessio <edoardodalessio27@gmail.com>
2025-06-29 14:42:59 +0000

    Changed client's restoring scripts for non verbose restoring

--- dsm/restorer.sh
+++ dsm/restorer.sh
@@ -1,11 +1,29 @@
-node_id=$1
+#!/bin/bash
 
-cd ~/demo/images
+if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
+  echo "Usage: $0 <app name> <number of thread> [--verbose]"
+  exit 1
+fi
 
-python3 ~/criu/dsm/thread_filter.py $node_id ;
+app=$1
+threads=$2
+verbose_flag=""
+
+if [ "$3" == "--verbose" ]; then
+  verbose_flag="-v"
+  
+fi
+
+
+cd ~/${app}/images || { echo "Image directory not found"; exit 1; }
+
+# Apply thread filtering
+python3 ~/criu/dsm/thread_filter.py "$threads" > /dev/null 2>&1
+
+# Optionally clear old pid
 rm -f /tmp/criu-restored.pid
 
-echo "Images changed"
-sudo ~/criu/criu/criu restore -v --shell-job --dsm_client 10.2.11.10;
-
-
+# Restore the application with CRIU
+touch /tmp/.restore_flag
+sudo ~/criu/criu/criu restore --shell-job --dsm_client 10.2.11.10 $verbose_flag
+rm -f /tmp/.restore_flag
